d5b99717e4c994db0bc1e56ca6fcd62c8132ab5f
Mfon Itohowo <mfonation29@gmail.com>
2023-03-29 21:49:19 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+var canvas = document.getElementById("canvas")
+var c = canvas.getContext("2d")
+
+const tx = 50;
+const ty = 20;
+
+// move the canvas by tx and ty
+context.translate(tx, ty);
+
+context.fillStyle = 'red';
+
+
+
